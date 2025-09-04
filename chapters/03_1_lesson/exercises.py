9c30be112fc73193995a1b498226727e232cdedf
oKudu56 <roccoknickrehm307@gmail.com>
2025-09-04 13:51:54 +0000

    week 3 classwork

--- chapters/03_1_lesson/exercises.py
+++ chapters/03_1_lesson/exercises.py
@@ -6,13 +6,75 @@ print("********** Ch 3 Exercise 1 **********")
 
 # Do your work for Exercise 1 here
 
-print("Ch 3 Exercise 1: Not implemented") # Delete this line when you write your code!
+
+def right_justify(input):
+    length = len(input)
+    print("length = ", length)
+    target = 70
+    spaces = target - length
+    space_string = ' ' *spaces
+    print(space_string + input)
+
+right_justify('error')
+
+
+
+
 
 
 
 print("********** Ch 3 Exercise 2 **********")
 
 # Do your work for Excercise 2 here.
+
+def do_twice(d):
+    d()
+    d()
+
+def print_spam():
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+    print('spam')
+
+do_twice(print_spam)
+
+
+
+
+
+
+
+
 
 print("Ch 3 Exercise 2: Not implemented") # Delete this line when you write your code!
 
--- chapters/03_1_lesson/flow_of_execution.py
+++ chapters/03_1_lesson/flow_of_execution.py
@@ -1,9 +1,6 @@
 # Try to run the code below. What happens?
 # Try editing the code to make it work.
 
-main()
-z
-
 
 print("Hello, World!")
 
