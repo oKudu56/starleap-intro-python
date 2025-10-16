4977a2b9d59c2108986b8b1c8cbaa1dbdeb38a96
Rocco Knickrehm <roccoknickrehm307@gmail.com>
2025-10-16 09:57:14 -0400

    Week 9 work

--- chapters/05_1_lesson/exercises.py
+++ chapters/05_1_lesson/exercises.py
@@ -21,13 +21,38 @@ print("Ch 5 Exercise 2: Not implemented") # Delete this line when you write your
 print("********** Ch 5 Exercise 3 **********")
 
 # Do your work for Exercise 3 here.
+def is_triangle(a,b,c):
+    print('is_triangle()',a,b,c)
+    if a >= b + c:
+        print('No')
+    elif b>= a + c:
+        print('No')
+    elif c>= b + a:
+        print('Yes')
+    else:
+        print("I don't lnow yet")
+is_triangle(1,7,10)
+is_triangle(1,2,20)
+is_triangle(3,6,40)
 
-print("Ch 5 Exercise 3: Not implemented") # Delete this line when you write your code!
-
+a= float(input('How long is side a7 '))
+print('a is', a, type(a))
+b = float(input('How long is side b7'))
+print('b is', b, type(b))
+c= float(input('How long is side c7 '))
+print('c is', c, type(c))
 
 
 print("********** Ch 5 Exercise 4 **********")
 
 # Do your work for Exercise 4 here.
+
+
+
+
+
+
+
+
 
 print("Ch 5 Exercise 4: Not implemented") # Delete this line when you write your code!
--- chapters/05_2_homework/hw1_1.py
+++ chapters/05_2_homework/hw1_1.py
@@ -6,13 +6,45 @@ print("********** Homework 1 Exercise 1 **********")
 
 # Do your work for Exercise 1 here
 
-print("Homework 1 Exercise 1: Not implemented") # Delete this line when you write your code!
+def print_tic_tac_toe():
+    v_row = (' ' *2 + '|')*2 + ' '*2
+    h_row = '-'*8
+    print(v_row)
+    print(h_row)
+    v_row = (' ' *2 + '|')*2 + ' '*2
+    h_row = '-'*8
+    print(v_row)
+    print(h_row)
+    v_row = (' ' *2 + '|')*2 + ' '*2
+    print(v_row)
+
+
+
+print_tic_tac_toe()
+
+
+
+
+
 
 
 
 print("********** Homework 1 Exercise 2 **********")
 
 # Do your work for Excercise 2 here
+ 
+fname = input('What is your first name? ')
+lname = input('What is your last name?')
+birth = input('When were you born?')
+mo = input('Month?')
+day = input('Day?')
+year = input('Year?')
+print('Hi, '+ fname + lname + ' born on ' + mo + ',' + day + year + '!')
+
+
+
+
+
 
 print("Homework 1 Exercise 2: Not implemented") # Delete this line when you write your code!
 
